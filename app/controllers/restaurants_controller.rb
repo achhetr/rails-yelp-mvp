46d12757942971df4f4fb9b53e2dfb44211ab7bf
akash chhetri <akash.chhetri1@gmail.com>
2021-02-24 13:09:33 +1100

    delete routes added, show method added to restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,20 @@
 class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: [:show, :destroy, :edit, :update]
+
   def index
     @restaurants = Restaurant.all
   end
   def show
     @restaurant = Restaurant.find(params[:id])
   end
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
 
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
