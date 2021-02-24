47e270cd6e2dd7efb84ee01ed32c2795ee52a67e
akash chhetri <akash.chhetri1@gmail.com>
2021-02-24 15:02:03 +1100

    reviews controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants, except: [:update, :edit]
+  resources :restaurants, except: [:update, :edit] do
+    resource :reviews, only: [:new, :create]
+  end
 end
