939d477f19e3f12047930c9edf1cca7d3cd5e4d7
akash chhetri <akash.chhetri1@gmail.com>
2021-02-24 14:52:48 +1100

    new restaurant controller#create added

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,10 +14,22 @@ class RestaurantsController < ApplicationController
   def new
     @restaurant = Restaurant.new
   end
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render :new
+    end
+  end
 
   private
 
   def find_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,5 +7,6 @@ class Restaurant < ApplicationRecord
 
   validates :name, presence: true
   validates :address, presence: true
+  validates :phone_number, presence: true
   validates :category, inclusion: { in: self.category}
 end
