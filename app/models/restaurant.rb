7e00044c502c3e9b10a72b9aaafba2a32c5c5d02
akash chhetri <akash.chhetri1@gmail.com>
2021-02-24 12:23:12 +1100

    faker seed created

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,8 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
+  # category = ["chinese", "italian", "japanese", "french", "belgian"]
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"]}
 end
