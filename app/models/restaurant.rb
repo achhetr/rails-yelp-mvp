12940d58f9225fe29d52bc13fc408c7baea672a6
akash chhetri <akash.chhetri1@gmail.com>
2021-02-24 14:58:03 +1100

    update and edit routes prevented

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,7 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   def self.category
-    ["CHINESE", "ITALIAN", "JAPANESE", "FRENCH", "BELGIAN"]
+    ["CHINESE", "ITALIAN", "JAPANESE", "FRENCH", "BELGIAN", "INDIAN"]
   end
 
   validates :name, presence: true
