6902e4c18d2911f29472c420f9e56c9413c88415
akash chhetri <akash.chhetri1@gmail.com>
2021-02-24 15:51:46 +1100

    review added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,23 @@
 class ReviewsController < ApplicationController
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
+    @review = Review.new(reviews_param)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(params[:restaurant_id])
+    else
+      render :new
+    end
   end
 
   private
 
   def reviews_param
-    param.require(:review).permit(:rating, :content)
+    params.require(:review).permit(:rating, :content)
   end
 end
